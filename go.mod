69438bbae89d8707e33d276683e5b1933f658934
jose <jose@katana1.com>
2018-09-04 22:27:04 +1000

    change

--- go.mod
+++ go.mod
@@ -1,4 +1,1 @@
 module github.com/joseg2/gorandom
-
-require (
-        )
---
+++ gorandom.go
@@ -0,0 +1,22 @@
+//  Random numbers
+//
+
+package gorandom
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func gorandom() {
+	const MAX = 999999999999
+
+	// This initializes a seed for 'rand' that will be different at every run, but predictable...
+	rand.Seed(time.Now().UnixNano())
+
+	// This returns a random integer from 1 to MAX
+	fmt.Println("__________________________________")
+	xx := rand.Intn(MAX)
+	fmt.Println("Random integer: ", xx)
+}
