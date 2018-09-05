651530120720bd36d700245ae461bcb5bdcf529c
jose <jose@katana1.com>
2018-09-05 11:16:46 +1000

    new function

--- gorandom.go
+++ gorandom.go
@@ -4,11 +4,15 @@
 package gorandom
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 )
 
+// Print a random 12 digit number
 func Random12digits() {
 	const MAX = 999999999999
 
@@ -21,7 +25,32 @@ func Random12digits() {
 	fmt.Println("Random integer: ", xx)
 }
 
+// From the numbers in the given pool, pseudo-random pick one and write it to &mynumber
 func RandomDigit(mynumber *int, pool int) {
 	rand.Seed(time.Now().UnixNano())
 	*mynumber = rand.Intn(pool)
+}
+
+// Print a random line from the given path
+func RandomLine(path string) {
+	var lines []string
+	var randy int
+
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	f.Close()
+
+	maxi := len(lines)
+
+	RandomDigit(&randy, maxi)
+
+	fmt.Println(lines[randy])
 }
